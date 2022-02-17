f0d09e22abbbcf701d147042cb4dc248843f38c1
ryanlanciaux <ryan.lanciaux@gmail.com>
2022-02-17 16:54:32 -0500

    Renames token

--- src/Olympus/Treasury.sol
+++ src/Olympus/Treasury.sol
@@ -192,7 +192,7 @@ interface IERC20Mintable {
   function mint( address account_, uint256 ammount_ ) external;
 }
 
-interface IOHMERC20 {
+interface ITHEOERC20 {
     function burnFrom(address account_, uint256 amount_) external;
 }
 
@@ -200,7 +200,7 @@ interface IBondCalculator {
   function valuation( address pair_, uint amount_ ) external view returns ( uint _value );
 }
 
-contract OlympusTreasury is Ownable {
+contract TheopetraTreasury is Ownable {
 
     using SafeMath for uint;
     using SafeERC20 for IERC20;
@@ -216,9 +216,9 @@ contract OlympusTreasury is Ownable {
     event ChangeQueued( MANAGING indexed managing, address queued );
     event ChangeActivated( MANAGING indexed managing, address activated, bool result );
 
-    enum MANAGING { RESERVEDEPOSITOR, RESERVESPENDER, RESERVETOKEN, RESERVEMANAGER, LIQUIDITYDEPOSITOR, LIQUIDITYTOKEN, LIQUIDITYMANAGER, DEBTOR, REWARDMANAGER, SOHM }
+    enum MANAGING { RESERVEDEPOSITOR, RESERVESPENDER, RESERVETOKEN, RESERVEMANAGER, LIQUIDITYDEPOSITOR, LIQUIDITYTOKEN, LIQUIDITYMANAGER, DEBTOR, REWARDMANAGER, STHEO }
 
-    address public immutable OHM;
+    address public immutable THEO;
     uint public immutable blocksNeededForQueue;
 
     address[] public reserveTokens; // Push only, beware false-positives.
@@ -260,36 +260,24 @@ contract OlympusTreasury is Ownable {
     mapping( address => bool ) public isRewardManager;
     mapping( address => uint ) public rewardManagerQueue; // Delays changes to mapping.
 
-    address public sOHM;
-    uint public sOHMQueue; // Delays change to sOHM address
+    address public sTHEO;
+    uint public sTHEOQueue; // Delays change to sTHEO address
 
     uint public totalReserves; // Risk-free value of all assets
     uint public totalDebt;
 
     constructor (
-        address _OHM,
-        address _DAI,
-        address _Frax,
-        address _OHMDAI,
+        address _THEO,
         uint _blocksNeededForQueue
     ) {
-        require( _OHM != address(0) );
-        OHM = _OHM;
-
-        isReserveToken[ _DAI ] = true;
-        reserveTokens.push( _DAI );
-
-        isReserveToken[ _Frax] = true;
-        reserveTokens.push( _Frax );
-
-       isLiquidityToken[ _OHMDAI ] = true;
-       liquidityTokens.push( _OHMDAI );
+        require( _THEO != address(0) );
+        THEO = _THEO;
 
         blocksNeededForQueue = _blocksNeededForQueue;
     }
 
     /**
-        @notice allow approved address to deposit an asset for OHM
+        @notice allow approved address to deposit an asset for THEO
         @param _amount uint
         @param _token address
         @param _profit uint
@@ -306,9 +294,9 @@ contract OlympusTreasury is Ownable {
         }
 
         uint value = valueOf(_token, _amount);
-        // mint OHM needed and store amount of rewards for distribution
+        // mint THEO needed and store amount of rewards for distribution
         send_ = value.sub( _profit );
-        IERC20Mintable( OHM ).mint( msg.sender, send_ );
+        IERC20Mintable( THEO ).mint( msg.sender, send_ );
 
         totalReserves = totalReserves.add( value );
         emit ReservesUpdated( totalReserves );
@@ -317,7 +305,7 @@ contract OlympusTreasury is Ownable {
     }
 
     /**
-        @notice allow approved address to burn OHM for reserves
+        @notice allow approved address to burn THEO for reserves
         @param _amount uint
         @param _token address
      */
@@ -326,7 +314,7 @@ contract OlympusTreasury is Ownable {
         require( isReserveSpender[ msg.sender ] == true, "Not approved" );
 
         uint value = valueOf( _token, _amount );
-        IOHMERC20( OHM ).burnFrom( msg.sender, value );
+        ITHEOERC20( THEO ).burnFrom( msg.sender, value );
 
         totalReserves = totalReserves.sub( value );
         emit ReservesUpdated( totalReserves );
@@ -347,7 +335,7 @@ contract OlympusTreasury is Ownable {
 
         uint value = valueOf( _token, _amount );
 
-        uint maximumDebt = IERC20( sOHM ).balanceOf( msg.sender ); // Can only borrow against sOHM held
+        uint maximumDebt = IERC20( sTHEO ).balanceOf( msg.sender ); // Can only borrow against sTHEO held
         uint availableDebt = maximumDebt.sub( debtorBalance[ msg.sender ] );
         require( value <= availableDebt, "Exceeds debt limit" );
 
@@ -384,18 +372,18 @@ contract OlympusTreasury is Ownable {
     }
 
     /**
-        @notice allow approved address to repay borrowed reserves with OHM
+        @notice allow approved address to repay borrowed reserves with THEO
         @param _amount uint
      */
-    function repayDebtWithOHM( uint _amount ) external {
+    function repayDebtWithTHEO( uint _amount ) external {
         require( isDebtor[ msg.sender ], "Not approved" );
 
-        IOHMERC20( OHM ).burnFrom( msg.sender, _amount );
+        ITHEOERC20( THEO ).burnFrom( msg.sender, _amount );
 
         debtorBalance[ msg.sender ] = debtorBalance[ msg.sender ].sub( _amount );
         totalDebt = totalDebt.sub( _amount );
 
-        emit RepayDebt( msg.sender, OHM, _amount, _amount );
+        emit RepayDebt( msg.sender, THEO, _amount, _amount );
     }
 
     /**
@@ -428,7 +416,7 @@ contract OlympusTreasury is Ownable {
         require( isRewardManager[ msg.sender ], "Not approved" );
         require( _amount <= excessReserves(), "Insufficient reserves" );
 
-        IERC20Mintable( OHM ).mint( _recipient, _amount );
+        IERC20Mintable( THEO ).mint( _recipient, _amount );
 
         emit RewardsMinted( msg.sender, _recipient, _amount );
     }
@@ -438,7 +426,7 @@ contract OlympusTreasury is Ownable {
         @return uint
      */
     function excessReserves() public view returns ( uint ) {
-        return totalReserves.sub( IERC20( OHM ).totalSupply().sub( totalDebt ) );
+        return totalReserves.sub( IERC20( THEO ).totalSupply().sub( totalDebt ) );
     }
 
     /**
@@ -463,15 +451,15 @@ contract OlympusTreasury is Ownable {
     }
 
     /**
-        @notice returns OHM valuation of asset
+        @notice returns THEO valuation of asset
         @param _token address
         @param _amount uint
         @return value_ uint
      */
     function valueOf( address _token, uint _amount ) public view returns ( uint value_ ) {
         if ( isReserveToken[ _token ] ) {
-            // convert amount to match OHM decimals
-            value_ = _amount.mul( 10 ** IERC20( OHM ).decimals() ).div( 10 ** IERC20( _token ).decimals() );
+            // convert amount to match THEO decimals
+            value_ = _amount.mul( 10 ** IERC20( THEO ).decimals() ).div( 10 ** IERC20( _token ).decimals() );
         } else if ( isLiquidityToken[ _token ] ) {
             value_ = IBondCalculator( bondCalculator[ _token ] ).valuation( _token, _amount );
         }
@@ -503,8 +491,8 @@ contract OlympusTreasury is Ownable {
             debtorQueue[ _address ] = block.number.add( blocksNeededForQueue );
         } else if ( _managing == MANAGING.REWARDMANAGER ) { // 8
             rewardManagerQueue[ _address ] = block.number.add( blocksNeededForQueue );
-        } else if ( _managing == MANAGING.SOHM ) { // 9
-            sOHMQueue = block.number.add( blocksNeededForQueue );
+        } else if ( _managing == MANAGING.STHEO ) { // 9
+            sTHEOQueue = block.number.add( blocksNeededForQueue );
         } else return false;
 
         emit ChangeQueued( _managing, _address );
@@ -614,9 +602,9 @@ contract OlympusTreasury is Ownable {
             result = !isRewardManager[ _address ];
             isRewardManager[ _address ] = result;
 
-        } else if ( _managing == MANAGING.SOHM ) { // 9
-            sOHMQueue = 0;
-            sOHM = _address;
+        } else if ( _managing == MANAGING.STHEO ) { // 9
+            sTHEOQueue = 0;
+            sTHEO = _address;
             result = true;
 
         } else return false;
