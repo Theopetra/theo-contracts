f00089830f1ac4ac780253ae6937130d907e0f7d
Jacob Krogman <jake@wlabel.co>
2022-02-25 10:17:12 -0500

    Update pragma

--- src/Interfaces/IBondCalculator.sol
+++ src/Interfaces/IBondCalculator.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-1.0
 
-pragma solidity 0.8.9;
+pragma solidity 0.7.5;
 
 interface IBondCalculator {
   function valuation( address pair_, uint amount_ ) external view returns ( uint _value );
