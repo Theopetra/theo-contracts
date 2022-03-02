f4baa47532d1cd6587f7d9a40b4b247ad71c82d9
s994n <65023744+s994n@users.noreply.github.com>
2022-03-02 22:30:26 +0000

    Move mocks deploy to separate script (#26)
    * fix: Move mocks deployment to seperate script
    
    * fix: Run formatter

--- src/Interfaces/IBondCalculator.sol
+++ src/Interfaces/IBondCalculator.sol
@@ -3,6 +3,5 @@
 pragma solidity 0.7.5;
 
 interface IBondCalculator {
-  function valuation( address pair_, uint amount_ ) external view returns ( uint _value );
+    function valuation(address pair_, uint256 amount_) external view returns (uint256 _value);
 }
-
