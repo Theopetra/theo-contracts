4509716cbbd901043ec762eb5f256a8d3106daa2
SimonN <65023744+s994n@users.noreply.github.com>
2022-03-04 13:55:12 +0000

    refactor: Update deploy script to move args for hardhat network

--- deploy/005_deploy_staking.ts
+++ deploy/005_deploy_staking.ts
@@ -9,24 +9,26 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
     const chainId = await getChainId();
-
+    
     const TheopetraAuthority = await deployments.get(CONTRACTS.authority);
 
     let epochLengthInBlocks;
     let firstEpochNumber;
     let firstEpochBlock;
-
-    // If on Hardhat network, use example values for testing
+    let args: any = [];
+    
+    // If on Hardhat network, use the following values for testing
     if (chainId === '1337') {
       epochLengthInBlocks = '2000';
       firstEpochNumber = '1';
       firstEpochBlock = '10';
+      args = [deployer, deployer, epochLengthInBlocks, firstEpochNumber, firstEpochBlock, TheopetraAuthority.address];
     }
 
     await deploy(CONTRACTS.staking, {
       from: deployer,
       log: true,
-      args: [deployer, deployer, epochLengthInBlocks, firstEpochNumber, firstEpochBlock, TheopetraAuthority.address],
+      args,
     });
   } catch (error) {
     console.log(error);
