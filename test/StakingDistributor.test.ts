4e2f51fa85261f1e1f0253611b3c42185eedcd9f
SimonN <65023744+s994n@users.noreply.github.com>
2022-03-11 17:06:06 +0000

    test: WIP add tests for distributor

--- test/StakingDistributor.test.ts
+++ test/StakingDistributor.test.ts
@@ -1,17 +1,26 @@
 import { expect } from './chai-setup';
 import { deployments, ethers, getNamedAccounts, getUnnamedAccounts } from 'hardhat';
 
+import { StakingDistributor__factory } from '../../next-app/src/typechain';
+
 import { setupUsers } from './utils';
-import { CONTRACTS } from '../utils/constants';
+import { CONTRACTS, MOCKS, MOCKSWITHARGS } from '../utils/constants';
+import { Contract } from 'ethers';
 
-const setup = deployments.createFixture(async () => {
-  await deployments.fixture([CONTRACTS.distributor, CONTRACTS.authority]);
-  const { deployer: owner } = await getNamedAccounts();
+const getContracts = async (): Promise<{ [key: string]: Contract }> => {
+  await deployments.fixture([CONTRACTS.distributor, CONTRACTS.authority, MOCKSWITHARGS.treasuryMock]);
 
-  const contracts = {
+  return {
     Distributor: await ethers.getContract(CONTRACTS.distributor),
     TheopetraAuthority: await ethers.getContract(CONTRACTS.authority),
+    TreasuryMock: await ethers.getContract(MOCKSWITHARGS.treasuryMock),
+    TheopetraERC20Mock: await ethers.getContract(MOCKS.theoTokenMock),
   };
+};
+
+const setup = deployments.createFixture(async () => {
+  const { deployer: owner } = await getNamedAccounts();
+  const contracts = await getContracts();
 
   const users = await setupUsers(await getUnnamedAccounts(), contracts);
 
@@ -23,10 +32,27 @@ const setup = deployments.createFixture(async () => {
   };
 });
 
-describe('Distributor', function () {
+describe.only('Distributor', function () {
+  let TreasuryMock: any;
+
   describe('Deployment', function () {
     it('can be deployed', async function () {
       await setup();
+    });
+
+    it('does not accept address zero as the treasury address', async function () {
+      const [owner] = await ethers.getSigners();
+      const { TreasuryMock, TheopetraERC20Mock, TheopetraAuthority } = await getContracts();
+      const epochLength = 2000;
+      const nextEpochBlock = 10;
+
+      const distributor = await new StakingDistributor__factory(owner).deploy(
+        TreasuryMock.address,
+        TheopetraERC20Mock.address,
+        epochLength,
+        nextEpochBlock,
+        TheopetraAuthority.address
+      );
     });
   });
 });
