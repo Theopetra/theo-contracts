94455ff80830d37556fa87c9280b7ddcb19c2794
s994n <65023744+s994n@users.noreply.github.com>
2022-04-11 15:25:25 +0100

    Bond depo v2 pricing (#50)
    * feat: Add scaffold for Treasury updates
    
    * feat: WIP update bond depo for pricing
    
    * feat: Update methods, libraries and interfaces for getting delta token price and delta treasury yield
    
    * feat: Updates to limit bond rate variable. WIP updates to use LP
    
    * feat: WIP update market price calculations, and test with mock bonding calculator
    
    * test: Update tests for market price
    
    * feat: WIP update market price calculations
    
    * feat: WIP updates to get market price
    
    * feat: Update types to allow larger deposits
    
    * feat: Update to set and use bonding calculator via bond depo
    
    * feat: Add setters for Drb and Dyb, and add associated tests
    
    * fix: Run formatter
    
    * fix: Update to fix running yarn gas report
    
    * fix: Move Olympus contracts out of src folder to avoid them getting compiled
    
    * fix: Small update to use addressZero when calling enable and not setting a bonding calculator
    
    * feat: Move bonding calculator getter and setter to Treasury, and update tests
    
    * feat: Update pendingFor to return additional info on notes. Add tests related to UI
    
    * fix: Run formatter
    
    * refactor: small changes to remove unused code
    
    * fix: Run formatter
    
    * refactor: Small correction to update comment
    
    * fix: Small correction to comment
    
    * fix: Fix delta token price calculation. Add comments. Update tests
    
    * fix: Small update to fix failing test
    
    * feat: Add discount to note.
    
    * test: Add test for bond event
    
    * fix: Update mock yield reporter
    
    * fix: Address review comment

--- src/Interfaces/IBondCalculator.sol
+++ src/Interfaces/IBondCalculator.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-1.0
 
-pragma solidity 0.7.5;
+pragma solidity >=0.7.5 <=0.8.10;
 
 interface IBondCalculator {
     function valuation(address pair_, uint256 amount_) external view returns (uint256 _value);
--- src/Interfaces/IYieldReporter.sol
+++ src/Interfaces/IYieldReporter.sol
@@ -2,10 +2,13 @@
 pragma solidity >=0.7.5;
 
 interface IYieldReporter {
-  event ReportYield(uint256 indexed id, int256 yield);
-  function lastYield() external view returns (int256);
-  function currentYield() external view returns (int256);
-  function getYieldById(uint256 id) external view returns (int256);
+    event ReportYield(uint256 indexed id, int256 yield);
 
-  function reportYield(int256 _amount) external returns (uint256);
+    function lastYield() external view returns (int256);
+
+    function currentYield() external view returns (int256);
+
+    function getYieldById(uint256 id) external view returns (int256);
+
+    function reportYield(int256 _amount) external returns (uint256);
 }
