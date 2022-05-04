3f05bc68cdf89daff508f93025a2a83a4d9fd2f4
SimonN <65023744+s994n@users.noreply.github.com>
2022-05-04 13:32:31 +0100

    feat: Add examples for contract setup

--- scripts/getTokenInfo.ts
+++ scripts/getTokenInfo.ts
@@ -1,15 +1,18 @@
-import hre from 'hardhat';
 import * as dotenv from 'dotenv';
-import { ethers } from 'hardhat';
+import { ethers, getUnnamedAccounts } from 'hardhat';
 import {TheopetraERC20Token__factory} from "../typechain-types";
 dotenv.config();
 
 const tokenNameAndSymbol = async () => {
-  // Connect to Ethereum test network (rinkeby, backed by third party provider Alchemy/Infura)
+  // Connect to Ethereum test network (rinkeby, using Alchemy as a backer below)
   // Default provider (via `getDefaultProvider`) is provided by ethers but highly throttled
   // const provider = hre.ethers.getDefaultProvider('rinkeby');
+  // E.g. with Metamask would be like:
+  // const provider = new ethers.providers.Web3Provider(window.ethereum)
+  // Using Alchemy below
   const provider = new ethers.providers.AlchemyProvider("rinkeby", process.env.ALCHEMY_API_KEY)
   // Call existing contract using contract's factory from typechain-types
+  // Address can be found in deployments/rinkeby folder
   const TheopetraERC20Token = TheopetraERC20Token__factory.connect("0x1A2EA28399A2e4f8f3EEfeA5f41770B4C61fE643", provider);
   // Call the methods `name` and `symbol` on the contract
   const contractName = await TheopetraERC20Token.name();
@@ -17,6 +20,21 @@ const tokenNameAndSymbol = async () => {
   console.log("Provider Network:", provider.network);
   console.log("Name >>>>> ", contractName);
   console.log("Symbol >>>>> ", contractSymbol);
+
+  // Get balance for a user
+  // First, get unnamed addresses
+  const [, bobAddress] = await getUnnamedAccounts();
+  console.log("Address For bob >>>>", bobAddress);
+  const bobBalance = await TheopetraERC20Token.balanceOf(bobAddress);
+  // Balance will be as a BigNumber
+  console.log("Bob THEO balance >>>>", bobBalance);
+  // Convert BigNumber to integer
+  console.log("Bob balance as Int", bobBalance.toNumber());
+
+  // How to connect the user to a contract
+  // Get the signer for the first unnamedAccount, for use with signing messages and transactions,
+  // and sending signed transactions to the network to execute state change operations
+  TheopetraERC20Token.connect(await ethers.getSigner(bobAddress));
 };
 
 const getTokenInfo = async () => {
---
+++ scripts/setupContracts.ts
@@ -0,0 +1,49 @@
+import hre from 'hardhat';
+import * as dotenv from 'dotenv';
+import { ethers, getUnnamedAccounts, getNamedAccounts } from 'hardhat';
+import {StakingDistributor__factory, STheopetra__factory, TheopetraAuthority__factory, TheopetraStaking__factory, TheopetraTreasury, TheopetraTreasury__factory} from "../typechain-types";
+dotenv.config();
+
+
+// Some contracts need permissions enabled on them, or need initializing
+// See `deploy/setup/setupIntegration.ts`
+const connectContracts = async () => {
+  // Connect to Ethereum test network using Alchemy as provider
+  const provider = new ethers.providers.AlchemyProvider("rinkeby", process.env.ALCHEMY_API_KEY)
+
+  // You can get the owner (same account) like this, as a signer:
+  const [owner, bob] = await ethers.getSigners();
+  // And/or get the address for the owner (named `deployer`) from getNamedAccounts
+  const { deployer } = await getNamedAccounts();
+  console.log('Owner/Deployer >>>>>', owner.address, bob.address, deployer);
+
+  // TheopetraAuthority and TheopetraAccessControlled have modifiers / methods for access control (e.g. `onlyVault`)
+  // E.g. Vault address has been pushed to Treasury as part of deployment
+  const TheopetraAuthority = TheopetraAuthority__factory.connect("0xBcdF034cE6624A817c1BfEffBDE8691443e5fDbB", provider);
+  const Treasury = TheopetraTreasury__factory.connect("0x6640C3FD53e4Cf446B4139f478A199147d663a44", provider);
+  console.log("Vault address (Treasury) >>>>", await TheopetraAuthority.vault(), Treasury.address);
+
+  // Other setup still needs to be done (see `deploy/setup/setupIntegration.ts`);
+  // E.g. sTheo Initialized (sTHEO totalSupply transfered to Staking contract, in preparation for Stakers)
+  const STheopetra = STheopetra__factory.connect("0xCD1a66F06eC36Db3F040C6065e5AAC0866FcD77A", provider);
+  const TheopetraStaking = TheopetraStaking__factory.connect("0x79b4882B3121061C054EEFEBcB05B2b3fFcf59Dd", provider);
+  console.log("Staking contract balance of sTHEO >>>>>", (await STheopetra.balanceOf(TheopetraStaking.address)).toNumber());
+
+// E.g. Setup for `Treasury.mint`
+// Set Distributor as reward manager in Treasury (to allow call to mint from Distributor when Rebasing)
+  const Distributor = StakingDistributor__factory.connect("0x0ee54Aa3fE9695Eff297582080Bd9766D09FBD9A", provider);
+  const addressZero = ethers.utils.getAddress('0x0000000000000000000000000000000000000000');
+  await Treasury.connect(owner).enable(8, Distributor.address, addressZero);
+  const isEnabled = await Treasury.permissions(8, Distributor.address);
+  console.log("Distributor as Reward Manager >>>>>>", isEnabled);
+};
+
+const setupContracts = async () => {
+  try {
+      await connectContracts();
+  } catch (err) {
+      console.log(err);
+  }
+};
+
+setupContracts();
