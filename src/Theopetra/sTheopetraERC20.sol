4d2550c6aaef1e7d9471285b6fea06f1fecffa6f
SimonN <65023744+s994n@users.noreply.github.com>
2022-05-09 13:34:48 +0100

    feat: Swap Existing use of onlyManager to onlyGuardian

--- src/Theopetra/sTheopetraERC20.sol
+++ src/Theopetra/sTheopetraERC20.sol
@@ -79,7 +79,7 @@ contract sTheopetra is IStakedTHEOToken, ITokenDebt, ERC20Permit, TheopetraAcces
         return true;
     }
 
-    function setIndex(uint256 _INDEX) external onlyManager returns (bool) {
+    function setIndex(uint256 _INDEX) external onlyGuardian returns (bool) {
         require(INDEX == 0);
         INDEX = gonsForBalance(_INDEX);
         return true;
