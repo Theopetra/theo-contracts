e2ecda4e997924bccbb738103d14eca4820cb298
SimonN <65023744+s994n@users.noreply.github.com>
2022-05-25 14:16:44 +0100

    feat: Add setup for public pre-list bond depo. Add waitFor to all setup function calls

--- deploy/setup/setupIntegration.ts
+++ deploy/setup/setupIntegration.ts
@@ -2,6 +2,7 @@ import { ethers } from 'hardhat';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { CONTRACTS, TESTWITHMOCKS } from '../../utils/constants';
 import { getContracts } from '../../utils/helpers';
+import { waitFor } from '../../test/utils';
 
 const func = async function (hre: HardhatRuntimeEnvironment) {
   try {
@@ -21,22 +22,23 @@ const func = async function (hre: HardhatRuntimeEnvironment) {
       pTheo,
       StakingLocked,
       FounderVesting,
+      PublicPreListBondDepository
     } = await getContracts();
 
     /* ======== Setup for `Treasury.mint` (when `TheopetraBondDepository.deposit` is called) ======== */
-    await TheopetraAuthority.pushVault(Treasury.address, true); // Push vault role to Treasury, to allow it to call THEO.mint
-    await sTheo.connect(owner).initialize(Staking.address, Treasury.address); // Initialize sTHEO
-    await pTheo.connect(owner).initialize(StakingLocked.address); // Initialize pTHEO
+    await waitFor(TheopetraAuthority.pushVault(Treasury.address, true)); // Push vault role to Treasury, to allow it to call THEO.mint
+    await waitFor(sTheo.connect(owner).initialize(Staking.address, Treasury.address)); // Initialize sTHEO
+    await waitFor(pTheo.connect(owner).initialize(StakingLocked.address)); // Initialize pTHEO
 
     /* ======== Setup for `Treasury.mint` (when `mint` is called on Treasury from StakingDistributor) ======== */
-    await Treasury.connect(owner).enable(8, Distributor.address, addressZero); // Set Distributor as reward manager in Treasury (to allow call to mint from Distributor when Rebasing)
+    await waitFor(Treasury.connect(owner).enable(8, Distributor.address, addressZero)); // Set Distributor as reward manager in Treasury (to allow call to mint from Distributor when Rebasing)
 
     /* ======== Setup for `Treasury.mint` (when `mint` is called on Treasury from StakingDistributor) ======== */
-    await Treasury.connect(owner).enable(8, Distributor.address, addressZero); // Set Distributor as reward manager in Treasury (to allow call to mint from Distributor when Rebasing)
+    await waitFor(Treasury.connect(owner).enable(8, Distributor.address, addressZero)); // Set Distributor as reward manager in Treasury (to allow call to mint from Distributor when Rebasing)
 
     /* ======== Other setup for `TheopetraBondDepository.deposit()` ======== */
-    await Treasury.connect(owner).enable(11, YieldReporter.address, addressZero); // Enable Yield Reporter in Treasury
-    await Treasury.connect(owner).enable(8, BondDepository.address, addressZero); // Set Bond Depo as reward manager in Treasury (to allow call to mint from NoteKeeper when adding new note)
+    await waitFor(Treasury.connect(owner).enable(11, YieldReporter.address, addressZero)); // Enable Yield Reporter in Treasury
+    await waitFor(Treasury.connect(owner).enable(8, BondDepository.address, addressZero)); // Set Bond Depo as reward manager in Treasury (to allow call to mint from NoteKeeper when adding new note)
 
     /* ======== Setup to allow Pushing Claim during `TheopetraBondDepository.redeem()` and `WhitelistTheopetraBondDepository.redeem()` ======== */
     // Set addresses of bond depos in staking, to allow bond depos to push claims to user when they redeem a note
@@ -45,6 +47,9 @@ const func = async function (hre: HardhatRuntimeEnvironment) {
 
     /* ======== Setup for Whitelist Bond Depository ======== */
     await Treasury.connect(owner).enable(8, WhitelistBondDepository.address, addressZero); // Set Whitelist Bond Depo as reward manager in Treasury (to allow call to mint from NoteKeeper when adding new note)
+
+    /* ======== Setup for Public Pre-List Bond Depository ======== */
+    await Treasury.connect(owner).enable(8, PublicPreListBondDepository.address, addressZero); // Set Public Pre-List Bond Depo as reward manager in Treasury (to allow call to mint from NoteKeeper when adding new note)
 
     /* ======== Setup for Founder Vesting ======== */
     await Treasury.connect(owner).enable(8, FounderVesting.address, addressZero); // Set Whitelist Founder Vesting as reward manager in Treasury (to allow call to mint)
