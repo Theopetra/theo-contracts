8013f8d8c300893685a46c46643d1b90f597c1b3
jk <34455148+dreadful-dev@users.noreply.github.com>
2022-06-07 10:57:31 -0400

    Jk/ttc 03/zero address validation (#156)
    * add comment explaining potential revert case
    
    * add zero address validation to TheopetraAuthority

--- src/Theopetra/TheopetraAuthority.sol
+++ src/Theopetra/TheopetraAuthority.sol
@@ -31,6 +31,8 @@ contract TheopetraAuthority is ITheopetraAuthority, TheopetraAccessControlled {
 
     address public newWhitelistSigner;
 
+    string private constant REQUIRE_ERROR = "Address cannot be zero address";
+
     /* ========== Constructor ========== */
 
     constructor(
@@ -41,16 +43,22 @@ contract TheopetraAuthority is ITheopetraAuthority, TheopetraAccessControlled {
         address _vault,
         address _whitelistSigner
     ) TheopetraAccessControlled(ITheopetraAuthority(address(this))) {
+        require(_governor != address(0), REQUIRE_ERROR);
         governor = _governor;
         emit GovernorPushed(address(0), governor, true);
+        require(_guardian != address(0), REQUIRE_ERROR);
         guardian = _guardian;
         emit GuardianPushed(address(0), guardian, true);
+        require(_policy != address(0), REQUIRE_ERROR);
         policy = _policy;
         emit PolicyPushed(address(0), policy, true);
+        require(_manager != address(0), REQUIRE_ERROR);
         manager = _manager;
         emit ManagerPushed(address(0), manager, true);
+        require(_vault != address(0), REQUIRE_ERROR);
         vault = _vault;
         emit VaultPushed(address(0), vault, true);
+        require(_whitelistSigner != address(0), REQUIRE_ERROR);
         whitelistSigner = _whitelistSigner;
         emit SignerPushed(address(0), whitelistSigner, true);
     }
@@ -58,36 +66,42 @@ contract TheopetraAuthority is ITheopetraAuthority, TheopetraAccessControlled {
     /* ========== GOV ONLY ========== */
 
     function pushGovernor(address _newGovernor, bool _effectiveImmediately) external onlyGovernor {
+        require(_newGovernor != address(0), REQUIRE_ERROR);
         if (_effectiveImmediately) governor = _newGovernor;
         newGovernor = _newGovernor;
         emit GovernorPushed(governor, newGovernor, _effectiveImmediately);
     }
 
     function pushGuardian(address _newGuardian, bool _effectiveImmediately) external onlyGovernor {
+        require(_newGuardian != address(0), REQUIRE_ERROR);
         if (_effectiveImmediately) guardian = _newGuardian;
         newGuardian = _newGuardian;
         emit GuardianPushed(guardian, newGuardian, _effectiveImmediately);
     }
 
     function pushPolicy(address _newPolicy, bool _effectiveImmediately) external onlyGovernor {
+        require(_newPolicy != address(0), REQUIRE_ERROR);
         if (_effectiveImmediately) policy = _newPolicy;
         newPolicy = _newPolicy;
         emit PolicyPushed(policy, newPolicy, _effectiveImmediately);
     }
 
     function pushManager(address _newManager, bool _effectiveImmediately) external onlyGovernor {
+        require(_newManager != address(0), REQUIRE_ERROR);
         if (_effectiveImmediately) manager = _newManager;
         newManager = _newManager;
         emit ManagerPushed(manager, newManager, _effectiveImmediately);
     }
 
     function pushVault(address _newVault, bool _effectiveImmediately) external onlyGovernor {
+        require(_newVault != address(0), REQUIRE_ERROR);
         if (_effectiveImmediately) vault = _newVault;
         newVault = _newVault;
         emit VaultPushed(vault, newVault, _effectiveImmediately);
     }
 
     function pushWhitelistSigner(address _newWhitelistSigner, bool _effectiveImmediately) external onlyGovernor {
+        require(_newWhitelistSigner != address(0), REQUIRE_ERROR);
         if (_effectiveImmediately) whitelistSigner = _newWhitelistSigner;
         newWhitelistSigner = _newWhitelistSigner;
         emit SignerPushed(whitelistSigner, newWhitelistSigner, _effectiveImmediately);
