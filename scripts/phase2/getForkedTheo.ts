a2853e7e17bb3ae3b24d8bc50547b77c5ebc2cb8
HomicidalChicken <86430321+Cryptofowl@users.noreply.github.com>
2023-03-02 21:31:49 -0500

    Added getTheo fork script

---
+++ scripts/phase2/getForkedTheo.ts
@@ -0,0 +1,46 @@
+import * as dotenv from 'dotenv';
+import hre from 'hardhat';
+import { ethers } from 'hardhat';
+import {address, abi} from '../../deployments/mainnet/TheopetraERC20Token.json';
+import {address as treasuryAddress} from '../../deployments/mainnet/TheopetraTreasury.json'
+import { BigNumber } from 'ethers';
+dotenv.config();
+
+const doTheTest = async () => {
+
+    await hre.network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [treasuryAddress],
+      });
+
+    await hre.network.provider.send("hardhat_setBalance", [
+        treasuryAddress,
+        "0x8ac7230489e80000",
+    ]);
+
+    let eth = await hre.network.provider.request({
+        method: "eth_getBalance",
+        params: [treasuryAddress],
+    });
+    
+    const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545/');
+
+    let signer = await provider.getSigner(treasuryAddress);
+    let contract = await ethers.getContractAt(abi, address, signer);
+
+    await contract.mint('0xAd72dEd03A5110c1807E68022D25c75E79B50eC5', 1000000000000000);
+
+    console.log(`Successfully minted $THEO to owner`);
+    let newBalance = await contract.balanceOf('0xAd72dEd03A5110c1807E68022D25c75E79B50eC5');
+    console.log(`$THEO Balance of address: 0xAd72dEd03A5110c1807E68022D25c75E79B50eC5 is ${newBalance}`)
+};
+
+const testIt = async () => {
+    try {
+        await doTheTest();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+testIt();
